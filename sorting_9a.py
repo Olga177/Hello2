90a81ced68f33d7e457d518b2543ac8f62ea3c11
Olga Kuznetsova <1774659@gmail.com>
2017-09-20 11:33:03 -0700

    Updated project with files related to hometask

--- sorting_9a.py
+++ sorting_9a.py
@@ -3,6 +3,10 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+
+
 def compareArrays (length,country_names,sorted_country_names): # Compare sorted and unsorted string arrays of the same length
     for i in (range(length)):
         if country_names[i] != sorted_country_names[i]:
@@ -14,6 +18,7 @@ driver = webdriver.Chrome()
 
 # 2. Open the  page to log in into admin
 driver.get("http://localhost/litecart/admin/")
+driver.implicitly_wait(20)
 # 2a. Set waiting time
 driver.implicitly_wait(30)
 # 2b.Find element with name "username"
@@ -43,14 +48,12 @@ countries_menu.click()
 #print("menu clicked")
 # 3c. Find page headline to be sure that page name is Countries
 headline = driver.find_element_by_css_selector("h1").text
-print("Headline found = ",headline)
+# print("Headline found = ",headline)
 
-
-# 4. Find  links to all contries on Contries page
+# 4. Find  links to all countries on Countries page
 country_links = driver.find_elements_by_css_selector("tr.row td a")  # Only odd elements are correct, even elements are not related
 amount_of_countries = len(country_links)
-print("Amount_of_countries = ", amount_of_countries)
-
+# print("Amount_of_countries = ", amount_of_countries)
 
 # 5. Find country names and put them in country_names (skipping even elements in country_links)
 country_names =[]
@@ -61,48 +64,108 @@ sorted_country_names = sorted(country_names)
 
 # 6. Compare sorted and not sorted lists
 countries_amount = len(country_names)
-print("Countries_amount =", countries_amount)
+#print("Countries_amount =", countries_amount)
 #print('sorted_country_names =', sorted_country_names)
 #print("country names",country_names)
 compareArrays(countries_amount,country_names, sorted_country_names)
 
-# 7. Find link to zones
+# 7. Find countries ============ with non zero zones
+# 7a. Find elements countries and for their zones
+driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+driver.implicitly_wait(20)
+# 7b. Find list of all countries on Country page and amount of those countries
+all_countries = driver.find_elements_by_xpath("//*[contains(@class,'row')]/td[5]/a")
+amount_of_all_countries = len(all_countries)
+# print("Amount of countries", amount_of_all_countries)
+# 7b. Find all elements related to country zones and amount of those elements
+all_contries_zones =driver.find_elements_by_xpath("//*[contains(@class,'row')]/td[6]")
+all_contries_codes = driver.find_elements_by_xpath("//*[contains(@class,'row')]/td[4]")
+amount_zone_elements = len(all_contries_zones)
+# print("amount_zone_element",amount_zone_elements )
+
+# 7c. Select countries with non-zero zone amount and put them into the list
+multiple_zones_countries =[]
+multiple_zones_contries_index =[]
+multiple_zones_codes =[]
+for i in range (amount_of_all_countries):
+    if all_contries_zones[i].text != '0':
+        multiple_zones_countries.append(all_countries[i].text)
+        multiple_zones_contries_index.append(i)
+        multiple_zones_codes.append(all_contries_codes[i].text)
+print("multiple zones countries = ",multiple_zones_countries)
+print("multiple_zones_contries_index = ", multiple_zones_contries_index)
+print("multiple_zones_codes = ", multiple_zones_codes)
+
+# 8. Find particular =============  country code for the country with multiple zones
+multiple_zones_codes_length = len(multiple_zones_codes)
+# print("multiple_zones_codes = ",multiple_zones_codes)
+for i in range (multiple_zones_codes_length):
+    country_code = multiple_zones_codes[i]
+    # print (" current country code = ",country_code)
+    # 8a. Go to ================= the page with zones for particular country
+    country_page_locator ="http://localhost/litecart/admin/?app=countries&doc=edit_country&country_code=" +country_code
+    print("country page locator = ", country_page_locator)
+    driver.get(country_page_locator)
+    driver.implicitly_wait(20)
+    #  8b. Find all zone names
+    zone_names = driver.find_elements_by_css_selector("table.dataTable tr td:nth-of-type(3) input")
+    # print("zone names = ", zone_names)
+    current_country_zones_length = len(zone_names)- 1
+    # print("current_country_zones_length", current_country_zones_length)
+    current_country_zones_names =[]
+    for i in range(current_country_zones_length):
+        # print("current zone name = ", zone_names[i].get_attribute("value"))
+        current_country_zones_names.append(zone_names[i].get_attribute("value"))
+        # print("current_country_zones_names = ", current_country_zones_names)
+        # print("length of current_country_zones_names = ",len(current_country_zones_names))
+
+    # 8c. Check zone names sorting for current country
+    sorted_current_country_zones_names = sorted(current_country_zones_names)
+    # print("length of sorted_current_country_zones_names = ",len(sorted_current_country_zones_names))
+    # print("sorted_current_country_zones_names", sorted_current_country_zones_names)
+    compareArrays(current_country_zones_length,current_country_zones_names,sorted_current_country_zones_names)
+
+# 9. Find link to zones menu
 geo_zones_menu = driver.find_element_by_css_selector("ul#box-apps-menu.list-vertical a[href*=geo_zones]")
 driver.implicitly_wait(20)
 # print('menu item found')
-# 7a. Click menu item to see page Geo Zones
+
+# 9a. Click menu item to see page Geo Zones
 geo_zones_menu.click()
 # print("menu clicked")
-# 7b. Find page headline
-headline = driver.find_element_by_css_selector("h1").text
-print("Headline found = ", headline)
 
-# 8 Find link to Canadian zones
+# 9b. Find page headline
+headline = driver.find_element_by_css_selector("h1").text
+# print("Headline found = ", headline)
+
+# 10 Find link to Canadian zones
 canadian_zones = driver.find_element_by_link_text("Canada")
 #print("canadian zones", canadian_zones)
-# 8a. Go to the page with Canadian zones
+# 10a. Go to the page with Canadian zones
 canadian_zones.click()
-# 8b.  Find page headline
+# 10b.  Find page headline
 headline = driver.find_element_by_css_selector("h1").text
-print("Headline found = ", headline)
-# 8c. Find list of Canadian zones
+# print("Headline found = ", headline)
+
+# 10c. Find list of Canadian zones
 canadian_zones_list = driver.find_elements_by_css_selector("select[name*=zone_code] option[selected*=selected]")
 #print("Canadian_zones_list",canadian_zones_list)
-# 8d. Find amount of Canadian zones
-canadian_zones_amount = len(canadian_zones_list)
-print("Canadian_zones_amount",canadian_zones_amount)
 
-# 9. Find Canadian zone names
+# 10d. Find amount of Canadian zones
+canadian_zones_amount = len(canadian_zones_list)
+# print("Canadian_zones_amount",canadian_zones_amount)
+
+# 11. Find Canadian zone names
 canadian_zones_names =[]
 for i in range(canadian_zones_amount):
     canadian_zones_names.append(canadian_zones_list[i].text)
 # print("Canadian_zones_names",canadian_zones_names)
 
-# 10. Sort Canadian zones names
+# 11a. Sort Canadian zones names
 sorted_canadian_zones_names = sorted(canadian_zones_names)
 # print("Sorted Canadian_zones_names",sorted_canadian_zones_names)
 
-# 11. Check that zone names are sorted
+# 11b. Check that zone names are sorted
 compareArrays(canadian_zones_amount,canadian_zones_names, sorted_canadian_zones_names)
 
 # 12 Checking if US zone names are sorted
@@ -111,23 +174,26 @@ geo_zones_menu = driver.find_element_by_css_selector("ul#box-apps-menu.list-vert
 driver.implicitly_wait(20)
 # 12b. Go to the gep-zones page
 geo_zones_menu.click()
+
 # 12c. Find page headline
 headline = driver.find_element_by_css_selector("h1").text
-print("Headline found = ", headline)
+# print("Headline found = ", headline)
 
 # 13. Find  a link to   US zones
 us_zones = driver.find_elements_by_css_selector("table.dataTable tr.row a[href*=id]")[2]
-print ("US link = ", us_zones.text)
+# print ("US link = ", us_zones.text)
 #print("us zones", us_zones)
+
 # 13a. Go to the page with  US zones
 us_zones.click()
 
 # 14. Find list of US zones
 us_zones_list = driver.find_elements_by_css_selector("select[name*=zone_code] option[selected*=selected]")
 #print("US_zones_list",us_zones_list)
+
 # 14a. Find amount of US zones
 us_zones_amount = len(us_zones_list)
-print("US_zones_amount",us_zones_amount)
+# print("US_zones_amount",us_zones_amount)
 
 # 15. Find US zone names
 us_zones_names =[]
@@ -143,10 +209,8 @@ sorted_us_zones_names = sorted(us_zones_names)
 compareArrays(us_zones_amount,us_zones_names, sorted_us_zones_names)
 
 
-
-
 # 17. Close the page
-driver.close()
+# driver.close()
 
 # 18. Close the browser
-driver.quit()
+# driver.quit()
